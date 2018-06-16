3c58c999f2a5f051bdf6ae037ff46b47064e2f09
maheshkareeya <maheshkareeya@gmail.com>
2018-06-15 22:41:56 -0700

    Mongodb connected with mlab

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // database: 'mongodb://maheshkareeya:mahesh619619Key@ds237610.mlab.com:37610/qcom',
-    database: 'mongodb://localhost:27017/qcom',
+    database: 'mongodb://maheshkareeya:mahesh619619Key@ds237610.mlab.com:37610/qcom',
+    // database: 'mongodb://localhost:27017/qcom',
 
     screte: 'yourscrete'
 }
